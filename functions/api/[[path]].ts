7558943b2d71c79fb0d0468466e0ebc4902bbd56
Daffa <daffabdullah111@gmail.com>
2026-06-26 14:08:56 +0700

    change domain backend

--- functions/api/[[path]].ts
+++ functions/api/[[path]].ts
@@ -6,13 +6,13 @@ interface PagesRequestContext {
 
 export const onRequest = async (context: PagesRequestContext) => {
   const url = new URL(context.request.url);
-  const backendBaseUrl = "https://budgeting-be.daffabdullah111.workers.dev/api/budgeting";
+  const backendBaseUrl = "https://budgeting-api.heydapps.com/api/budgeting";
   const targetPath = url.pathname.replace(/^\/api/, "");
-  
+
   const targetUrl = `${backendBaseUrl}${targetPath}${url.search}`;
-  
+
   // Clone the request with the new target URL to preserve method, headers, and body
   const modifiedRequest = new Request(targetUrl, context.request);
-  
+
   return fetch(modifiedRequest);
 };
